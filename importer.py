fabdb5778ac6e78a4dafe2ddff7bfbdb9266921c
lrcascaldi <41606653+lrcascaldi@users.noreply.github.com>
2018-07-24 02:31:47 -0300

    Add files via upload

---
+++ importer.py
@@ -0,0 +1,26 @@
+import csv
+import re
+import pandas
+
+#importa CSVs
+
+class Parte:
+    def __init__(self, )
+
+with open ('dataone.csv', 'r') as Parte:
+    readCSV_Parte = csv.reader(Parte, delimiter=',')
+    print("Iniciando: Parte")
+    
+    #for row in readCSV_Parte:
+       # print(row)
+
+print("\n")
+
+with open ('twodata.csv', 'r') as Contra:
+    readCSV_Contra = csv.reader(Contra, delimiter=',')
+    print("Iniciando: Contra-Parte")
+    
+    #for row in readCSV_Contra:
+       # print(row)
+
+
